9a83ee51fb4ec3b83cadd0c2fb774fde5236aec0
Zehan Khan <zkhan@hootboard.com>
2023-06-14 01:52:39 +0530

    feat: :sparkles: infinity scroll added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,33 @@ import { For, createSignal, onMount, createEffect } from 'solid-js';
 import axios from 'axios';
 import EpisodeBox from './components/EpisodeBox';
 
+
 const fetchEpisodes = async () =>
   await axios.get("https://rickandmortyapi.com/api/episode"); // The API
 
 function App() {
   const [episodes, setEpisodes] = createSignal(null);
+
+  const fetchNextData = async () => {
+    if (episodes()?.info?.next) {
+      const { data } = await fetchEpisodes(episodes()?.info.next);
+      const modifiedEpisodes = [...episodes()?.results, ...data.results];
+      setEpisodes((prevState) => ({
+        ...prevState,
+        results: modifiedEpisodes,
+        info: data.info,
+      }));
+    }
+  };
+  const handleScroll = () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+      fetchNextData();
+    }
+  };
+  
+  createEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  });
 
   onMount(async () => {
     setEpisodes((await fetchEpisodes()).data);
